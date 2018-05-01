cca1eb6400a199e66bb4cc94ceb8310f0e09a88d
ChrisDilts <chrisdilts@gmail.com>
2018-05-01 17:08:09 -0400

    loc stuff

---
+++ loc/loc.ps1
@@ -0,0 +1,19 @@
+﻿import-csv E:\temp\NameList.csv | ForEach-Object {Get-ADPrincipalGroupMembership -Identity $_.username | Select-Object name | Where-Object {$_.name -match "FIM-LOC*"}} | Out-File -Append E:\Temp\ListOut.csv
+
+$OutArray = New-Object PSObject
+import-csv E:\temp\NameList.csv | ForEach-Object {
+$user = $_.username
+$lob={Get-ADPrincipalGroupMembership -Identity $_.username | Select-Object name | Where-Object {$_.name -match "FIM-LOC*"}}
+Add-Member -InputObject $OutArray -MemberType NoteProperty -Name User -Value $user -Force
+Add-Member -InputObject $OutArray -MemberType NoteProperty -Name LOB -Value $lob -Force
+}
+$OutArray | Out-File E:\Temp\ListOut.csv
+
+
+
+
+
+import-csv E:\temp\NameList.csv
+ForEach-Object {
+Get-ADPrincipalGroupMembership -Identity $_.username | Select-Object name | Where-Object {$_.name -match "FIM-LOC*"}
+}
