650875f8653128c41096b8666aa98368fe8a3aba
Chris Dilts <chrisdilts@gmail.com>
2018-12-25 16:59:24 -0500

    Add DynamoDB example

---
+++ Python/UpdateMovie.py
@@ -0,0 +1,38 @@
+from __future__ import print_function # Python 2/3 compatibility
+import boto3
+import json
+import decimal
+
+# Helper class to convert a DynamoDB item to JSON.
+class DecimalEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, decimal.Decimal):
+            if o % 1 > 0:
+                return float(o)
+            else:
+                return int(o)
+        return super(DecimalEncoder, self).default(o)
+
+dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
+
+table = dynamodb.Table('Movies')
+
+title = "The Ballad of Buster Scruggs"
+year = 2018
+
+response = table.update_item(
+    Key={
+        'year': year,
+        'title': title
+    },
+    UpdateExpression="set info.rating = :r, info.plot=:p, info.actors=:a",
+    ExpressionAttributeValues={
+        ':r': decimal.Decimal(5.5),
+        ':p': "An anthology film comprising six stories, each dealing with a different aspect of life in the Old West.",
+        ':a': ["Tim Blake Nelson", "Willie Watson", "Clancy Brown"]
+    },
+    ReturnValues="UPDATED_NEW"
+)
+
+print("UpdateItem succeeded:")
+print(json.dumps(response, indent=4, cls=DecimalEncoder))
